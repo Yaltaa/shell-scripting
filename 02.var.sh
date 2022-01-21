b5ac1c5a910c951bf5a288c5d80049697f41511f
yatlaa <mohanyatlaa@gmail.com>
2022-01-21 20:02:46 +0530

    present status change

--- 02.var.sh
+++ 02.var.sh
@@ -4,7 +4,7 @@ student_name='ravan'
 echo student_name = $student_name
 echo student_name = ${student_name}
 
-student_status'present'
+student_status='present'
 
-echo student_status = $yes
-echo student_status = ${yes}
+echo student_status = $student_status
+echo student_status = ${student_status}
