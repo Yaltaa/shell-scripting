309c9e721c4da352b6374a02a722a533d7c8f156
yatlaa <mohanyatlaa@gmail.com>
2022-01-22 16:08:23 +0530

    download content

--- Roboshop-project/components/frontend.sh
+++ Roboshop-project/components/frontend.sh
@@ -34,3 +34,6 @@
 
 echo installing nginx
 yum install nginx -y >/tmp/roboshop.log
+
+echo "Download frontend conteny"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
