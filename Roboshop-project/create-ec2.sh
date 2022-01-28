09dc5719e0580a7779fed2bb4207961304081d0b
yatlaa <mohanyatlaa@gmail.com>
2022-01-28 18:44:40 +0530

    else conditions with colours

--- Roboshop-project/create-ec2.sh
+++ Roboshop-project/create-ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"  --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
-  echo "Unable to find the Image AMI_ID"
+  echo -e "\e[1;31mUnable to find the Image AMI_ID\e[0m"
 else
-  echo AMI_ID = ${AMI_ID}
+  echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
 fi
