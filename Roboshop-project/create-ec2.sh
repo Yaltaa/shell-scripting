a387eb56682b7077243d41baa9ff35fc86dd56d9
yatlaa <mohanyatlaa@gmail.com>
2022-01-28 19:27:24 +0530

    Tag name added N changes withe new changes

--- Roboshop-project/create-ec2.sh
+++ Roboshop-project/create-ec2.sh
@@ -12,4 +12,6 @@ else
   echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
 fi
 
+aws ec2 describe-instances --filters Name=tag:Name,Values=frontend --query 'Reservations[*].Instances[*].PrivateIpAddress'
+
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
