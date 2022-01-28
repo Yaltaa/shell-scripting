d13789664b0a8a216331878099c4f35e1d8b2ca6
yatlaa <mohanyatlaa@gmail.com>
2022-01-28 18:31:59 +0530

    change in if condition are changed by new condition

--- Roboshop-project/create-ec2.sh
+++ Roboshop-project/create-ec2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"  --query 'Images[*].[ImageId]' --output text)
-if [ -z "${AMI_ID}" ]; than
-  echo "Unable to find image AMID"
+if [ -z "${AMI_ID}"]; then
+  echo "Unable to find the Image AMI_ID"
 fi
