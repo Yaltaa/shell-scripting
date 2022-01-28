11bc64e4356d13c3b269163beda24db2021319e7
yatlaa <mohanyatlaa@gmail.com>
2022-01-28 18:38:18 +0530

    else condition

--- Roboshop-project/create-ec2.sh
+++ Roboshop-project/create-ec2.sh
@@ -2,4 +2,6 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"  --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
   echo "Unable to find the Image AMI_ID"
+else
+  AMI_ID = ${AMI_ID}
 fi
