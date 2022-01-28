04946bc6018b02438ddcbe144aec57732affc1ba
yatlaa <mohanyatlaa@gmail.com>
2022-01-28 19:11:00 +0530

    Tag name added

--- Roboshop-project/create-ec2.sh
+++ Roboshop-project/create-ec2.sh
@@ -12,4 +12,4 @@ else
   echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${INSTANCE_NAME}}]"
