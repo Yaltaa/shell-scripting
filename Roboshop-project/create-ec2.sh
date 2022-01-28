1d3edfecf36e66c97db9165a49ac47a29df63d4f
yatlaa <mohanyatlaa@gmail.com>
2022-01-28 18:15:03 +0530

    first comment

--- Roboshop-project/create-ec2.sh
+++ Roboshop-project/create-ec2.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-INSTANCE_NAME$1
-if [ -z "${INSTANCE_NAME}" ]; then
-  echo -e "\e[33mInstance Name Argument is need\e[0m"
-  exit
+AMI=ID$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"  --query 'Images[*].[ImageId]' --output text)
+if [ -z "${AMI_ID}" ]; than
+echo "Unable to find image AMID"
 fi
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text
-)
-if [ -z "${AMI_ID}" ]; then
-  echo -e "\e[1;31munable to find image AMI_ID\e[0m"
-else
-  echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
-fi
-
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
