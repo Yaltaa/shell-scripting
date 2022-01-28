0bb1a3213130713c739c3ce24474cb21979b50f2
yatlaa <mohanyatlaa@gmail.com>
2022-01-28 19:50:50 +0530

    Tag name added N changes withe new changes done

--- Roboshop-project/create-ec2.sh
+++ Roboshop-project/create-ec2.sh
@@ -8,10 +8,17 @@ fi
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"  --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
   echo -e "\e[1;31mUnable to find the Image AMI_ID\e[0m"
+  exit
 else
   echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+if [ -z "${Private_IP}"]; then
+ aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+else
+  echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
+exit
+fi
+
